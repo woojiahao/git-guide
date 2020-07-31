eb15a02896bcb8ad155263f7f1e1577b7fb14f77
woojiahao <woojiahao1234@gmail.com>
2020-07-31 17:31:35 +0800

    Setup Gatsby for site

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,10 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `chapters`,
+        path: `${__dirname}/content/`,
+      }
+    }
+  ],
 }
---
+++ site/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const Index = () => (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+)
+
+export default Index
