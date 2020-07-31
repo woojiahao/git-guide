c97508b9563a86d9cbc6b95b07df25a0d50b59a1
woojiahao <woojiahao1234@gmail.com>
2020-07-31 17:28:55 +0800

    Add script to copy chapters to site/content

---
+++ copy-chapters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+content_path='./site/content/chapters/'
+
+# Locate all folders in the current director, filter only chapter folders, store values into an array
+readarray -d '' -t chapters < <(find . -regextype sed -maxdepth 1  -type d -regex '.*' | grep './[[:digit:]]')
+
+for i in "${chapters[@]}"
+do
+  mkdir -p $content_path
+  cp -r $i $content_path
+done
