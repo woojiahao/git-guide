66482788c4907930a264faeebfdb6811b534174d
woojiahao <woojiahao1234@gmail.com>
2020-07-31 18:59:20 +0800

    Set mkdir to only create once per run

--- copy-chapters.sh
+++ copy-chapters.sh
@@ -2,11 +2,12 @@
 
 content_path='./site/content/chapters/'
 
+mkdir -p $content_path
+
 # Locate all folders in the current director, filter only chapter folders, store values into an array
 readarray -d '' -t chapters < <(find . -regextype sed -maxdepth 1  -type d -regex '.*' | grep './[[:digit:]]')
 
 for i in "${chapters[@]}"
 do
-  mkdir -p $content_path
   cp -r $i $content_path
 done
