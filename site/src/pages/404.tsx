27b2ca43c7956a439e93038c8acb99eb80e0276e
woojiahao <woojiahao1234@gmail.com>
2020-08-01 11:10:45 +0800

    Add 404 page

---
+++ site/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../components/layout/layout"
+
+export default () => {
+  return (
+    <Layout pageTitle='Page not found!'>
+      <p>The page you have requested is not found!</p>
+    </Layout>
+  )
+}
